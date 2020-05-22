47a9ca7dc48ed224b5cf4fa4eebf30c8b494146e
lesley <lesleybanadzem2017@gmail.com>
2020-05-22 11:29:51 -0700

    added some basic comments for more clarification

--- Server/src/utils/jwt.js
+++ Server/src/utils/jwt.js
@@ -1,24 +1,22 @@
-const jwt = require("jsonwebtoken")
-const JWT_SECRETE=  "273146745YJFMJDABFLJKUSHGDFLNB";
+const jwt = require("jsonwebtoken");
+const JWT_SECRETE = "273146745YJFMJDABFLJKUSHGDFLNB";
 
 /**
- * 
+ *
  * @param {token} userInfo token to be verified and converted to userId and email
  */
-function verifyToken (userInfo){
-
-    return jwt.verify(userInfo, JWT_SECRETE);
+function verifyToken(userInfo) {
+  return jwt.verify(userInfo, JWT_SECRETE);
 }
 
-
-function signToken ({userId, email}){
-    return  jwt.sign({userId, email}, JWT_SECRETE, {
-        expiresIn: "7 days",
-    })
+function signToken({ userId, email }) {
+  return jwt.sign({ userId, email }, JWT_SECRETE, {
+    expiresIn: "7 days",
+  });
 }
 
 module.exports = {
-    signToken,
-    JWT_SECRETE,
-    verifyToken
-}
+  signToken,
+  JWT_SECRETE,
+  verifyToken,
+};
