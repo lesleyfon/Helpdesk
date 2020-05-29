001a071864746e8fb9fefcaae97c3d860fd1fed9
lesley <lesleybanadzem2017@gmail.com>
2020-05-29 12:17:06 -0700

    created the Reistration page and Added a route so it displays on /register

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,21 @@
 import React from "react";
+
+//Router
+import { Route } from "react-router-dom";
+//Components
+import Registration from "./Page/Registration/Registration";
+// Styles
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">Help Desk</header>
+      <Route
+        path="/register"
+        exact
+        component={(props) => <Registration {...props} />}
+      />
     </div>
   );
 }
