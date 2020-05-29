e4071619ce303ae32ddf3bad5ce48bb959ce8c0a
lesley <lesleybanadzem2017@gmail.com>
2020-05-29 12:32:07 -0700

    Created a basic logic for rendering login and signup components based which ever component you want to render

--- client/src/Page/Registration/Registration.jsx
+++ client/src/Page/Registration/Registration.jsx
@@ -1,7 +1,31 @@
 import React, { Component } from "react";
 
+//Components
+import LogIn from "../../Components/LogIn/LogIn";
+import SignUp from "../../Components/SignUp/SignUp";
+
 export default class Registration extends Component {
+  state = {
+    login: true,
+  };
   render() {
-    return <div>Registration</div>;
+    const { login } = this.state;
+
+    return (
+      <div className="registration-container">
+        <div className="registration-header">
+          {login ? "Member Login" : "member Registration"}
+        </div>
+        <div className="registration-main">
+          {login ? <LogIn /> : <SignUp />}
+        </div>
+        <div
+          className="registration-footer"
+          onClick={() => this.setState({ login: !login })}
+        >
+          {login ? "Create an Account" : "Login"}
+        </div>
+      </div>
+    );
   }
 }
