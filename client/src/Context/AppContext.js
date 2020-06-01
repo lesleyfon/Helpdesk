8fd91728e6e299900d37ace243bceb1471113912
lesley <lesleybanadzem2017@gmail.com>
2020-05-31 21:14:39 -0700

    Added some basic comments

--- client/src/Context/AppContext.js
+++ client/src/Context/AppContext.js
@@ -2,7 +2,10 @@ import { createContext } from "react";
 
 const AppContext = createContext();
 
+// Provider - The component that provides the value
 export const AppContextProvider = AppContext.Provider;
+
+// Consumer - A component that is consuming the value
 export const AppContextConsumer = AppContext.Consumer;
 
 export default AppContext;
