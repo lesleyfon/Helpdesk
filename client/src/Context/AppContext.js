6028c74b45570edfb603a2347d353535eee4cc57
lesley <lesleybanadzem2017@gmail.com>
2020-05-31 21:13:36 -0700

    Initialzed context to be used by child components

---
+++ client/src/Context/AppContext.js
@@ -0,0 +1,8 @@
+import { createContext } from "react";
+
+const AppContext = createContext();
+
+export const AppContextProvider = AppContext.Provider;
+export const AppContextConsumer = AppContext.Consumer;
+
+export default AppContext;
