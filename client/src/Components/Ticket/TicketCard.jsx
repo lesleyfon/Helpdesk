9bb6c3cbc759d939ab11bb7948176bfe5ecbb641
lesley <lesleybanadzem2017@gmail.com>
2020-06-08 21:56:52 -0700

    Updated the deled mutation so it renders only tickets that haven't been deleted using the cache

--- client/src/Components/Ticket/TicketCard.jsx
+++ client/src/Components/Ticket/TicketCard.jsx
@@ -1,7 +1,21 @@
 import React from "react";
 import { MdDelete } from "react-icons/md";
 import { Mutation } from "react-apollo";
-import { DELETE_MUTATION } from "./../../GraphQL/Queries";
+import { DELETE_MUTATION, GET_TICKETS_QUERY } from "./../../GraphQL/Queries";
+
+//The first Params is the cache object which we can use to read and write data
+// The second Params id the data that we get from what ever function we just executed
+const updateCache = (cache, { data: { deleteTicket } }) => {
+  // Get all tickets from the cache
+  const { allTickets } = cache.readQuery({ query: GET_TICKETS_QUERY });
+
+  cache.writeQuery({
+    query: GET_TICKETS_QUERY,
+    data: {
+      allTickets: allTickets.filter((ticket) => ticket.id !== deleteTicket.id),
+    },
+  });
+};
 function TicketCard({ ticket }) {
   //Format the ticket Date to a readable String
   const date = new Date(Number(ticket.created_at));
@@ -16,7 +30,7 @@ function TicketCard({ ticket }) {
       <div className="ticket-details">
         <div className="title-info">
           <p className="ticket-title"> Question: {ticket.title}</p>
-          <Mutation mutation={DELETE_MUTATION}>
+          <Mutation mutation={DELETE_MUTATION} update={updateCache}>
             {(deleteMutation) => {
               return (
                 <MdDelete
