a3760fbeba85dc056a46223faf4869e5da548d39
lesley <lesleybanadzem2017@gmail.com>
2020-06-09 22:05:29 -0700

    Added migrations for tickets and seeds

--- Server/src/data/migrations/20200609213148_helpdesk.js
+++ Server/src/data/migrations/20200609213148_helpdesk.js
@@ -9,8 +9,26 @@ exports.up = async function (knex) {
       table.bigInteger("phone_number");
       table.string("password").notNullable();
     });
+  await knex.schema
+    .raw('CREATE EXTENSION IF NOT EXISTS "uuid-ossp"')
+    .createTable("ticket", (table) => {
+      table.uuid("id").defaultTo(knex.raw("uuid_generate_v4()")).primary();
+      table.string("title").notNullable();
+      table.text("description", "longtext");
+      table.string("category").notNullable();
+      table.timestamp("created_at", { useTz: true }).defaultTo(knex.fn.now());
+      table
+        .uuid("created_by")
+        .unsigned()
+        .notNullable()
+        .references("id")
+        .inTable("user")
+        .onUpdate("CASCADE")
+        .onDelete("CASCADE");
+    });
 };
 
 exports.down = async function (knex) {
+  await knex.schema.dropTableIfExists("ticket");
   await knex.schema.dropTableIfExists("user");
 };
---
+++ Server/src/data/seeds/02_tickets.js
@@ -0,0 +1,12 @@
+exports.seed = async (knex) => {
+  await knex("ticket").insert([
+    {
+      id: "c0456bfc-0038-4b0c-b2c8-433c65b4dae2",
+      title: "Front end Test/ Test Failing ",
+      description:
+        "font end test failing after mocking redux store and react router dom",
+      category: "FrontEnd test",
+      [`created_by`]: "6846bf46-c568-4abc-a89a-686d46437f2a",
+    },
+  ]);
+};
