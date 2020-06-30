517f3ab3fe1b8d511f310930027a5ac5f93f0ff6
lesley <lesleybanadzem2017@gmail.com>
2020-06-30 11:23:45 -0700

    Minor server fixes

--- Server/knexfile.js
+++ Server/knexfile.js
@@ -1,38 +1,38 @@
 const { DB_NAME, DB_PASSWORD, USER } = require("./configVariables");
 
 const connection = {
-  database: DB_NAME,
-  user: USER,
-  password: DB_PASSWORD,
+	database: DB_NAME,
+	user: USER,
+	password: DB_PASSWORD,
 };
 const knexSeedMigration = {
-  migrations: {
-    directory: "./src/data/migrations",
-  },
-  seeds: {
-    directory: "./src/data/seeds",
-  },
-  useNullAsDefault: true,
+	migrations: {
+		directory: "./src/data/migrations",
+	},
+	seeds: {
+		directory: "./src/data/seeds",
+	},
+	useNullAsDefault: true,
 };
 
 module.exports = {
-  development: {
-    client: "pg",
-    ...knexSeedMigration,
-    connection: {
-      ...connection,
-    },
-  },
+	development: {
+		client: "pg",
+		...knexSeedMigration,
+		connection: {
+			...connection,
+		},
+	},
 
-  production: {
-    client: "pg",
-    ...knexSeedMigration,
-    connection: {
-      ...connection,
-    },
-    pool: {
-      min: 2,
-      max: 10,
-    },
-  },
+	production: {
+		client: "pg",
+		...knexSeedMigration,
+		connection: {
+			...connection,
+		},
+		pool: {
+			min: 2,
+			max: 10,
+		},
+	},
 };
