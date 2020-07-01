28e598c0a8a9814cb24909b8c43d372fb7abac3d
lesley <lesleybanadzem2017@gmail.com>
2020-07-01 11:03:41 -0700

    Implemented logging out a user

--- client/src/Components/Header/Header.jsx
+++ client/src/Components/Header/Header.jsx
@@ -1,39 +1,46 @@
 import React, { Component } from "react";
-
+import { withRouter } from "react-router-dom";
 // Styles
 import "./Header.css";
-export default class Header extends Component {
-  render() {
-    return (
-      <nav className="navbar navbar-expand-lg navbar-mainbg">
-        <div className="navbar-brand navbar-logo">Help Desk</div>
+import { AUTH_TOKEN } from "../../constants";
 
-        <div className="collapse navbar-collapse" id="navbarSupportedContent">
-          <ul className="navbar-nav ml-auto">
-            <div className="hori-selector">
-              <div className="left"></div>
-              <div className="right"></div>
-            </div>
+function Header({ history }) {
+	// Log out a user
+	const logout = () => {
+		localStorage.removeItem(AUTH_TOKEN);
+		history.push("/register");
+	};
+	return (
+		<nav className="navbar navbar-expand-lg navbar-mainbg">
+			<div className="navbar-brand navbar-logo">Help Desk</div>
 
-            <li className="nav-item">
-              <i className="fas fa-tachometer-alt"></i>Dashboard
-            </li>
+			<div className="collapse navbar-collapse" id="navbarSupportedContent">
+				<ul className="navbar-nav ml-auto">
+					<div className="hori-selector">
+						<div className="left"></div>
+						<div className="right"></div>
+					</div>
 
-            <li className="nav-item">
-              <i className="far fa-clone"></i>Add a Ticket
-            </li>
-            <li className="nav-item">
-              <i className="far fa-calendar-alt"></i>Profile
-            </li>
-            <li className="nav-item">
-              <i className="far fa-chart-bar"></i>Tickets
-            </li>
-            <li className="nav-item active">
-              <i className="far fa-address-book"></i>About Us
-            </li>
-          </ul>
-        </div>
-      </nav>
-    );
-  }
+					<li className="nav-item">
+						<i className="fas fa-tachometer-alt"></i>Dashboard
+					</li>
+
+					<li className="nav-item">
+						<i className="far fa-clone"></i>Add a Ticket
+					</li>
+					<li className="nav-item">
+						<i className="far fa-calendar-alt"></i>Profile
+					</li>
+					<li className="nav-item">
+						<i className="far fa-chart-bar"></i>Tickets
+					</li>
+					<li className="nav-item active logout" onClick={logout}>
+						<i className="far fa-address-book"></i>Log Out
+					</li>
+				</ul>
+			</div>
+		</nav>
+	);
 }
+
+export default withRouter(Header);
