dda727cce22d29a56986bb7f142ce5b21297cca7
lesley <lesleybanadzem2017@gmail.com>
2020-07-07 21:26:20 -0700

    Displayed ticket information on the page

--- client/src/Page/Question/Question.jsx
+++ client/src/Page/Question/Question.jsx
@@ -8,14 +8,29 @@ class Question extends Component {
 			location: { pathname },
 		} = this.props;
 
-		const [_, __, id, title] = pathname.split("/");
+		const [, , id] = pathname.split("/"); // the [ , , ] represents the first two elements in the array. because we don't need those variables and need just the id
+
 		return (
 			<main>
 				<div className="Ticket ">
 					<Query query={Get_TICKET_AT_ID} variables={{ id: id }}>
 						{({ loading, error, data }) => {
-							console.log(data);
-							return <h1>Hello World</h1>;
+							if (loading) return <h1>Fetching data</h1>;
+
+							const { fetchTicket } = data;
+
+							let date = new Date(Number(fetchTicket.created_at)); // Format date. Returns a date Object
+							date = `${date.toLocaleDateString()}`; // Convert it to local string
+							console.log(fetchTicket);
+							return (
+								<section>
+									<h2>{fetchTicket.title}</h2>
+									<h5>Date: {date}</h5>
+									<h5>By {fetchTicket.created_by.first_name} </h5>
+									<p>Description: {fetchTicket.description}</p>
+									<p>Category: {fetchTicket.category}</p>
+								</section>
+							);
 						}}
 					</Query>
 				</div>
@@ -23,5 +38,4 @@ class Question extends Component {
 		);
 	}
 }
-
 export default Question;
