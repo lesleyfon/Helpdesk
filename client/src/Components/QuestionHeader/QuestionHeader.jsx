d0d262cc021844f56a6da44ad4b2cdf3f3aac1d2
lesley <lesleybanadzem2017@gmail.com>
2020-07-21 14:57:08 -0700

    Moved the question jsx form the question page to its own file and created couple more components to render answers for each question

---
+++ client/src/Components/QuestionHeader/QuestionHeader.jsx
@@ -0,0 +1,41 @@
+import React, { Component } from "react";
+import { Query } from "react-apollo";
+import { Get_TICKET_AT_ID } from "./../../GraphQL/Queries.js";
+class QuestionHeader extends Component {
+	render() {
+		const {
+			location: { pathname },
+		} = this.props;
+
+		const [, , id] = pathname.split("/"); // the [ , , ] represents the first two elements in the array. because we don't need those variables and need just the id
+
+		return (
+			<section className="question">
+				<div className="question_ticket">
+					<Query query={Get_TICKET_AT_ID} variables={{ id: id }}>
+						{({ loading, error, data }) => {
+							if (loading) return <h1>Fetching data</h1>;
+
+							const { fetchTicket } = data;
+
+							let date = new Date(Number(fetchTicket.created_at)); // Format date. Returns a date Object
+							date = `${date.toLocaleDateString()}`; // Convert it to local string
+							console.log(fetchTicket);
+							return (
+								<section>
+									<h2>{fetchTicket.title}</h2>
+									<h5>Date: {date}</h5>
+									<h5>By {fetchTicket.created_by.first_name} </h5>
+									<p>Description: {fetchTicket.description}</p>
+									<p>Category: {fetchTicket.category}</p>
+								</section>
+							);
+						}}
+					</Query>
+				</div>
+			</section>
+		);
+	}
+}
+
+export default QuestionHeader;
