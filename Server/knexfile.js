d730377ea4f38d6a1889ea8f0162def6c46ee9ce
lesley <lesleybanadzem2017@gmail.com>
2020-09-10 09:49:43 -0700

    remove all files from the src folder to the root of the folder for the server files

--- Server/knexfile.js
+++ Server/knexfile.js
@@ -1,15 +1,14 @@
-// const { DATABASE_URL } = require("./src/configVariables.js");
 let path = __dirname;
 path = path.split("/src").join("").trim() + "/.env";
 
-const dotenv = require("dotenv").config({ path: path });
+require("dotenv").config({ path: path });
 
 const knexSeedMigration = {
 	migrations: {
-		directory: "./src/data/migrations",
+		directory: "./data/migrations",
 	},
 	seeds: {
-		directory: "./src/data/seeds",
+		directory: "./data/seeds",
 	},
 	useNullAsDefault: true,
 };
